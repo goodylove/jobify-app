1891c0a2d3ba85e87048c2bd157d20fb13e97b94
goodylove <goodyc474@gmail.com>
2025-03-31 03:07:54 +0100

    chore: install  packages

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("server is working");
+import express from "express";
+
+const PORT = 5100;
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("helo world");
+});
+
+const start = () => {
+  app.listen(PORT, () => console.log("Listening to port " + PORT));
+};
+
+start();
