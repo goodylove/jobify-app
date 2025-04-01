da13e49f587fa80a1583d69a8d9632d24dc65358
goodylove <goodyc474@gmail.com>
2025-04-01 01:24:25 +0100

    feat: add update job route

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import { nanoid } from "nanoid";
 
-import express from "express";
+import express, { raw } from "express";
 
 const PORT = process.env.PORT || 5100;
 const app = express();
@@ -49,6 +49,42 @@ app.post("/api/v1/jobs", (req, res) => {
   const job = { id: nanoid(), company, position };
   jobs.push(job);
   res.status(200).json({ jobs });
+});
+
+app.get("/api/v1/jobs/:id", (req, res) => {
+  const { id } = req.params;
+
+  const singleJob = jobs?.find((job) => job.id === id);
+
+  if (!singleJob) {
+    res.status(400).json({ message: "invalid Id" });
+    return;
+  }
+
+  res.status(200).json({ singleJob });
+});
+
+app.patch("/api/v1/jobs/:id", (req, res) => {
+  const { company, position } = req.body;
+
+  if (!company || !position) {
+    res.status(400).json({ message: "please provide company and position" });
+    return;
+  }
+
+  const { id } = req.params;
+
+  const job = jobs?.find((job) => job.id === id);
+
+  if (!job) {
+    res.status(400).json({ message: "invalid Id" });
+    return;
+  }
+  job.company = company;
+  job.position = position;
+  job.id = nanoid();
+
+  res.status(200).json({ message: "Jobs modifield " });
 });
 
 const start = () => {
