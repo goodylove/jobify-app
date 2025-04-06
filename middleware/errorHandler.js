49835ef4786756995c4f853a5fcaf42866566982
goodylove <goodyc474@gmail.com>
2025-04-06 23:16:14 +0100

    fiix bug

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,5 +1,17 @@
+import { StatusCodes } from "http-status-codes";
+
 function ErrorHandlerMiddleware(err, req, res, next) {
-  console.log(err);
+  let customErr = {
+    message: err.message || "internal server error",
+    statusCode: StatusCodes.INTERNAL_SERVER_ERROR || 500,
+  };
+
+  if ((err.name = "CastError")) {
+    customErr.message = "";
+    customErr.statusCode = StatusCodes.NOT_FOUND;
+  }
+
+  res.status(customErr.statusCode).json({ msg: customErr.message, data: null });
 }
 
 export default ErrorHandlerMiddleware;
