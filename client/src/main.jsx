4f222f831fea3e7e6d80493c540b9d8b3843cdb5
goodylove <goodyc474@gmail.com>
2025-04-21 13:35:46 +0100

    fix  :client bug

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import  customFetch from './utils/customFetch.js'
+
+
+
+const res =  await customFetch.get('/test')
+console.log(res)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
